e087bdc04d09cd883287d71300662cb35b0a097f
Clement Rey <cr.rey.clement@gmail.com>
2016-11-12 13:41:40 +0100

    04 > implemented assembly multiplier

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,22 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+(MAIN)
+	@R2
+	M=0
+
+(MULTLOOP)
+	@R0
+	MD=M-1
+	@EXIT
+	D;JLT
+	@R1
+	D=M
+	@R2
+	M=M+D
+	@MULTLOOP
+	0;JMP
+
+(EXIT)
+	@EXIT
+	0;JMP
