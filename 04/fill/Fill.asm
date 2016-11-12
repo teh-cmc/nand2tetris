61a56bc5b2643e1aec714e94dccbd30938bf2aed
Clement Rey <cr.rey.clement@gmail.com>
2016-11-12 19:48:33 +0100

    04 > implemented assembly kbd-controlled screen painter

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,48 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(MAIN)
+
+(TICKLOOP)
+	@KBD
+	D=M
+	@PAINTBLACK
+	D;JGT
+	@PAINTWHITE
+	0;JMP
+
+(PAINTBLACK)
+	@R0
+	M=-1
+	@PAINT
+	0;JMP
+(PAINTWHITE)
+	@R0
+	M=0
+	@PAINT
+	0;JMP
+
+(PAINT)
+	@SCREEN
+	D=A
+	@pixel
+	M=D
+(PAINTLOOP)
+	@R0
+	D=M
+	@pixel
+	A=M
+	M=D
+	@pixel
+	MD=M+1
+	@KBD
+	D=A-D
+	@PAINTLOOP
+	D;JGT
+	@TICKLOOP
+	0;JMP
